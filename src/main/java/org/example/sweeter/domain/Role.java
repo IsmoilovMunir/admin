6e866a783a6d78b177e7471ba6e309a58ba61d36
ismoi <ismoilov.munir97@gmail.com>
2021-04-29 12:56:54 +0300

    commit1

---
+++ src/main/java/org/example/sweeter/domain/Role.java
@@ -0,0 +1,5 @@
+package org.example.sweeter.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/org/example/sweeter/repos/UserRepo.java
@@ -0,0 +1,8 @@
+package org.example.sweeter.repos;
+
+import org.example.sweeter.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
